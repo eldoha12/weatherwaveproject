0360b78b7bd8c84e804c6dd135781333db75e884
eldoha12 <elfadili.doha.el@gmail.com>
2024-12-11 20:48:00 +0100

    ajoutt1_doha

---
+++ reset2.js
@@ -0,0 +1,7 @@
+<form class="reset-form" action="success.html" method="get">
+  <div class="input-group">
+    <label for="reset-email">Adresse e-mail</label>
+    <input type="email" id="reset-email" name="reset-email" placeholder="Entrez votre e-mail" required></input>
+  </div>
+  <button type="submit" class="login-button">Envoyer le lien de réinitialisation</button>
+</form>

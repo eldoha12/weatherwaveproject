0f6cb62d58ebb949c7372197d7b0fe369974d268
eldoha12 <elfadili.doha.el@gmail.com>
2025-01-04 20:19:12 +0100

    inscri_email_auto_doha

--- public/inscription/inscription.js
+++ public/inscription/inscription.js
@@ -33,3 +33,23 @@ document.querySelector('.signup-form').addEventListener('submit', async (e) => {
       alert("Une erreur est survenue lors de l'inscription.");
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const nameInput = document.getElementById("name");
+    const emailInput = document.getElementById("email");
+  
+    nameInput.addEventListener("input", () => {
+      const fullName = nameInput.value.trim().toLowerCase();
+      if (fullName) {
+        // Remplace les espaces par des points et enlève les caractères spéciaux
+        const sanitizedFullName = fullName
+          .replace(/\s+/g, ".") // Remplace les espaces par des points
+          .replace(/[^a-zA-Z.]/g, ""); // Enlève les caractères non autorisés
+        const generatedEmail = `${sanitizedFullName}@weatherwave.com`;
+        emailInput.value = generatedEmail;
+      } else {
+        emailInput.value = ""; // Efface l'email si le nom est vide
+      }
+    });
+  });
+  

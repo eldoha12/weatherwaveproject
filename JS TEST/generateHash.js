0553fea07abbc6440370a5729c4a18dc2c00073a
eldoha12 <elfadili.doha.el@gmail.com>
2025-01-07 16:43:28 +0100

    tache_finale_doha

--- JS TEST/generateHash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-
-const password = '123456'; // Entere ici Le mot de passe à hacher
-
-bcrypt.hash(password, 10, (err, hashedPassword) => {
-  if (err) throw err;
-  console.log('Mot de passe haché:', hashedPassword);
-});
